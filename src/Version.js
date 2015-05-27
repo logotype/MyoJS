308c7779ed824b04416381cc0d7f171ef07e4687
Victor Norgren <victor@logotype.se>
2015-05-27 22:35:31 +0800

    0.13.0

--- src/Version.js
+++ src/Version.js
@@ -1,7 +1,7 @@
 // This file is automatically updated from package.json by grunt.
 module.exports = {
-    full: '0.8.7',
+    full: '0.13.0',
     major: 0,
-    minor: 8,
-    dot: 7
+    minor: 13,
+    dot: 0
 };
